e13981c371e5e93e1ac22050d31a75720422b815
jkaplanis <johntkaplanis@gmail.com>
2020-03-19 19:43:27 -0700

    Created object to store user inputs in the local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,73 +1,63 @@
+var hoursData = JSON.parse(localStorage.getItem("hoursData"));
+if (hoursData === null) {
+    hoursData = {
+        "9": "",
+        "10": "",
+        "11": "",
+        "12": "",
+        "13": "",
+        "14": "",
+        "15": "",
+        "16": "",
+        "17": ""
+    };
+}
+
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+   
+    var key = $(this).parent().attr("id").split("-")[1];
+    var userInput = $(this).parent().find("textarea").val(); 
+    hoursData[key] = userInput;
+    localStorage.setItem("hoursData", JSON.stringify(hoursData));
+});
+
+// check localStorage for hours data
+// update hoursData if localStorage for hours data isn't empty
+// how to determine if time is future/present/past
+// let currentHour be the hour of the current time of day
+// get reference to all the hours
+
 // display date at the top of the display
 var dateHeader = moment().format('dddd, MMMM Do, YYYY');
 $("#currentDay").text(dateHeader);
-// save the current hour to a variable 
 
+// save the current hour to a variable 
 var currentHour = moment().hour();
 
 
 
 var hoursArray = $(".time-block")
 
-for(var i = 0; i < hoursArray.length; i++){
+for (var i = 0; i < hoursArray.length; i++) {
     var hoursID = parseInt($(hoursArray[i]).attr("id").split("-")[1]);
     // $(".time-block").removeClass("past present future")
-    var test = $(hoursArray[i]).attr("id")
-    
-    if (hoursID < currentHour){
-        
-        $("#" + test).addClass("past")
+    var timeBlockDiv = $(hoursArray[i]).attr("id")
+
+    //  get description from hoursData
+    var description = hoursData[hoursID.toString()];
+    // display description form hours data
+    $("#" + timeBlockDiv).find("textarea").val(description);
+
+    // compare curent hour to hours ID and display correct color
+    if (hoursID < currentHour) {
+        $("#" + timeBlockDiv).addClass("past")
     }
-    else if(hoursID === currentHour){
-        // $(".time-block").removeClass("past")
-        // $(".time-block").removeClass("future")
-        $("#" + test).addClass("present")
+    else if (hoursID === currentHour) {
+        $("#" + timeBlockDiv).addClass("present")
     }
-    
-    else{
-        // $(".time-block").removeClass("past")
-        // $(".time-block").removeClass("present")
-        $("#" + test).addClass("future")
+    else {
+        $("#" + timeBlockDiv).addClass("future")
     };
 };
 
-
-// if time block hour is before current hour from moment (saved as variable)
-// then the block with be grey
-
-// if time block hour is equal to current hour from moment
-// then the time block is red
-
-// if time block is later than current hour from moment
-// then the time block is green (future)
-
-// if text is entered in timeblock and save is clicked (.on ("click", function(){}))
-// then add to local storage
-
-// var parentID;
-// var inputText;
-// var displayText;
-
-// $(".saveBtn").on("click", function(){
-//     parentID = $(this).parent().attr("id")
-//     inputText = $(".description").val();
-//     localStorage.setItem("input", JSON.stringify(inputText));
-// })
-// displayText = JSON.parse(localStorage.getItem("input"));
-// $("parentID > textarea").text(displayText);
-var parentID;
-var inputText;
-$(".saveBtn").on("click", handleSave);
-
-function handleSave(event) {
-    parentID = $(this).parent().attr("id")
-    console.log(parentID);
-    inputText = $(".description").val();
-    localStorage.setItem(parentID, JSON.stringify(inputText));
-
-}
-
-
-// when they refresh the page persists (get from local storage, append, text)
-
-//  past = grey present = red future = green
