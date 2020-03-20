d24065c42c66f575e03e7e17020fc7830b40e995
jkaplanis <johntkaplanis@gmail.com>
2020-03-19 19:53:59 -0700

    Improved comments in code

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,7 @@
+
+// Check if user input exists in the local storage and add it to the 
+// variable hoursData. If there is nothing then create and at the keys
+// matching the available hours. 
 var hoursData = JSON.parse(localStorage.getItem("hoursData"));
 if (hoursData === null) {
     hoursData = {
@@ -13,40 +17,34 @@ if (hoursData === null) {
     };
 }
 
+// List for clicks on the save buttons, add the input to the hoursData
+// object and add to local storage. 
 $(".saveBtn").on("click", function (event) {
     event.preventDefault();
-   
+
     var key = $(this).parent().attr("id").split("-")[1];
-    var userInput = $(this).parent().find("textarea").val(); 
+    var userInput = $(this).parent().find("textarea").val();
     hoursData[key] = userInput;
     localStorage.setItem("hoursData", JSON.stringify(hoursData));
 });
 
-// check localStorage for hours data
-// update hoursData if localStorage for hours data isn't empty
-// how to determine if time is future/present/past
-// let currentHour be the hour of the current time of day
-// get reference to all the hours
-
-// display date at the top of the display
+// Display date at the top of the page
 var dateHeader = moment().format('dddd, MMMM Do, YYYY');
 $("#currentDay").text(dateHeader);
 
-// save the current hour to a variable 
+// Save the current hour to a variable 
 var currentHour = moment().hour();
 
-
-
+// Create an array of all of the time-blocks
 var hoursArray = $(".time-block")
 
+// Run a for loop to cycle through each item in the array, 
+// add data from local storage, compare current hour to 
+// hour id, assign colors to represent past, present, or future
 for (var i = 0; i < hoursArray.length; i++) {
     var hoursID = parseInt($(hoursArray[i]).attr("id").split("-")[1]);
-    // $(".time-block").removeClass("past present future")
     var timeBlockDiv = $(hoursArray[i]).attr("id")
-
-    //  get description from hoursData
     var description = hoursData[hoursID.toString()];
-    // display description form hours data
     $("#" + timeBlockDiv).find("textarea").val(description);
 
     // compare curent hour to hours ID and display correct color
